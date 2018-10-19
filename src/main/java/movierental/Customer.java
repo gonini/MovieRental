fc71b16968d2165410c03fc77fb0cddcccf31d61
GongUi <zhanggoni@gmail.com>
2018-10-19 17:04:49 +0900

    replace magic number with symbolic constant

--- src/main/java/movierental/Customer.java
+++ src/main/java/movierental/Customer.java
@@ -5,6 +5,15 @@ import java.util.List;
 
 class Customer {
 
+    private static final int LONG_TERM_RENTAL_DAYS_BY_REGULAR = 2;
+    private static final int LONG_TERM_RENTAL_DAYS_BY_CHILDRENS = 3;
+
+    private static final int DEFAULT_PRICE_BY_REGULAR = 2;
+    private static final double DEFAULT_PRICE_BY_CHILDRENS = 1.5;
+    private static final int DEFAULT_PRICE_FACTOR_BY_NEW_RELEASE = 3;
+
+    private static final double LONG_TERM_RENTAL_COST_FACTOR = 1.5;
+
     private String _name;
     private List<Rental> _rentals = new ArrayList<>();
 
@@ -31,17 +40,17 @@ class Customer {
             //determine amounts for each line
             switch (each.getMovie().getPriceCode()) {
                 case regular:
-                    thisAmount += 2;
-                    if (each.getDaysRented() > 2)
-                        thisAmount += (each.getDaysRented() - 2) * 1.5;
+                    thisAmount += DEFAULT_PRICE_BY_REGULAR;
+                    if (each.getDaysRented() > LONG_TERM_RENTAL_DAYS_BY_REGULAR)
+                        thisAmount += (each.getDaysRented() - LONG_TERM_RENTAL_DAYS_BY_REGULAR) * LONG_TERM_RENTAL_COST_FACTOR;
                     break;
                 case newRelease:
-                    thisAmount += each.getDaysRented() * 3;
+                    thisAmount += each.getDaysRented() * DEFAULT_PRICE_FACTOR_BY_NEW_RELEASE;
                     break;
                 case childrens:
-                    thisAmount += 1.5;
-                    if (each.getDaysRented() > 3)
-                        thisAmount += (each.getDaysRented() - 3) * 1.5;
+                    thisAmount += DEFAULT_PRICE_BY_CHILDRENS;
+                    if (each.getDaysRented() > LONG_TERM_RENTAL_DAYS_BY_CHILDRENS)
+                        thisAmount += (each.getDaysRented() - LONG_TERM_RENTAL_DAYS_BY_CHILDRENS) * LONG_TERM_RENTAL_COST_FACTOR;
                     break;
             }
 
