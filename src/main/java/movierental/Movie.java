f260387c21ba2258ca1dde9336bfb24d631948c0
GongUi <zhanggoni@gmail.com>
2018-10-27 17:36:00 +0900

    move method

--- src/main/java/movierental/Movie.java
+++ src/main/java/movierental/Movie.java
@@ -1,6 +1,17 @@
 package movierental;
 
 class Movie {
+
+    private static final int LONG_TERM_RENTAL_DAYS_BY_REGULAR = 2;
+    private static final int LONG_TERM_RENTAL_DAYS_BY_CHILDRENS = 3;
+
+    private static final int DEFAULT_PRICE_BY_REGULAR = 2;
+    private static final double DEFAULT_PRICE_BY_CHILDRENS = 1.5;
+    private static final int DEFAULT_PRICE_FACTOR_BY_NEW_RELEASE = 3;
+
+    private static final double LONG_TERM_RENTAL_COST_FACTOR = 1.5;
+
+
     private String _title;
     private PriceCode _priceCode;
 
@@ -16,7 +27,38 @@ class Movie {
     public void setPriceCode(PriceCode priceCode) {
         _priceCode = priceCode;
     }
+
     public String getTitle() {
         return _title;
+    }
+
+    public double getPrice(int daysRented) {
+        double price = 0;
+
+        //determine amounts for each line
+        switch (getPriceCode()) {
+            case regular:
+                price += DEFAULT_PRICE_BY_REGULAR;
+                if (daysRented > LONG_TERM_RENTAL_DAYS_BY_REGULAR)
+                    price += (daysRented - LONG_TERM_RENTAL_DAYS_BY_REGULAR) * LONG_TERM_RENTAL_COST_FACTOR;
+                break;
+            case newRelease:
+                price += daysRented * DEFAULT_PRICE_FACTOR_BY_NEW_RELEASE;
+                break;
+            case childrens:
+                price += DEFAULT_PRICE_BY_CHILDRENS;
+                if (daysRented > LONG_TERM_RENTAL_DAYS_BY_CHILDRENS)
+                    price += (daysRented - LONG_TERM_RENTAL_DAYS_BY_CHILDRENS) * LONG_TERM_RENTAL_COST_FACTOR;
+                break;
+        }
+        return price;
+    }
+
+
+    public int getFrequentRenterPoints(int daysRented) {
+        int frequentRenterPoints = 1;
+        if ((getPriceCode() == PriceCode.newRelease) && daysRented > 1)
+            frequentRenterPoints++;
+        return frequentRenterPoints;
     }
 }
--- src/main/java/movierental/Rental.java
+++ src/main/java/movierental/Rental.java
@@ -5,68 +5,34 @@ package movierental;
  */
 class Rental implements ProductRental {
 
-    private static final int LONG_TERM_RENTAL_DAYS_BY_REGULAR = 2;
-    private static final int LONG_TERM_RENTAL_DAYS_BY_CHILDRENS = 3;
-
-    private static final int DEFAULT_PRICE_BY_REGULAR = 2;
-    private static final double DEFAULT_PRICE_BY_CHILDRENS = 1.5;
-    private static final int DEFAULT_PRICE_FACTOR_BY_NEW_RELEASE = 3;
-
-    private static final double LONG_TERM_RENTAL_COST_FACTOR = 1.5;
-
-    private Movie _movie;
-    private int _daysRented;
+    private Movie movie;
+    private int daysRented;
 
     Rental(Movie movie, int daysRented) {
-        _movie = movie;
-        _daysRented = daysRented;
+        this.movie = movie;
+        this.daysRented = daysRented;
     }
 
     private int getDaysRented() {
-        return _daysRented;
-    }
-
-    private PriceCode getPriceCode() {
-        return _movie.getPriceCode();
+        return daysRented;
     }
 
     private String getTitle() {
-        return _movie.getTitle();
+        return movie.getTitle();
     }
 
      Movie getMovie() {
-        return _movie;
+        return movie;
     }
 
     @Override
     public double getAmounts() {
-            double thisAmount = 0;
-
-            //determine amounts for each line
-            switch (getPriceCode()) {
-                case regular:
-                    thisAmount += DEFAULT_PRICE_BY_REGULAR;
-                    if (getDaysRented() > LONG_TERM_RENTAL_DAYS_BY_REGULAR)
-                        thisAmount += (getDaysRented() - LONG_TERM_RENTAL_DAYS_BY_REGULAR) * LONG_TERM_RENTAL_COST_FACTOR;
-                    break;
-                case newRelease:
-                    thisAmount += getDaysRented() * DEFAULT_PRICE_FACTOR_BY_NEW_RELEASE;
-                    break;
-                case childrens:
-                    thisAmount += DEFAULT_PRICE_BY_CHILDRENS;
-                    if (getDaysRented() > LONG_TERM_RENTAL_DAYS_BY_CHILDRENS)
-                        thisAmount += (getDaysRented() - LONG_TERM_RENTAL_DAYS_BY_CHILDRENS) * LONG_TERM_RENTAL_COST_FACTOR;
-                    break;
-            }
-        return thisAmount;
+        return movie.getPrice(getDaysRented());
     }
 
     @Override
     public int getFrequentRenterPoints() {
-        int frequentRenterPoints = 1;
-        if ((getPriceCode() == PriceCode.newRelease) && getDaysRented() > 1)
-            frequentRenterPoints++;
-        return frequentRenterPoints;
+        return movie.getFrequentRenterPoints(daysRented);
     }
 
     @Override
